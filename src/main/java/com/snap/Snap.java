6b3d4550ca3f7a75deafb6d04002ad5ab2d058c5
Jay <JJ.Buckby@hotmail.co.uk>
2023-01-16 15:13:05 +0000

    Added player messages

--- src/main/java/com/snap/Snap.java
+++ src/main/java/com/snap/Snap.java
@@ -13,6 +13,7 @@ public class Snap extends CardGame {
             System.out.println("Game is restarting");
             snapGame();
         } else {
+            String playersTurn = "";
             faceMatch = true;
         }
     }
@@ -20,9 +21,11 @@ public class Snap extends CardGame {
     public void snapGame() {
         createDeck();
         sortCards(CardSorting.shuffle);
-        System.out.println("First Card is being dealt, to deal more cards, press Enter, to claim a match, you must type snap.");
+        System.out.println("\uD83C\uDD56\uD83C\uDD50\uD83C\uDD5C\uD83C\uDD54 \uD83C\uDD58\uD83C\uDD5D\uD83C\uDD58\uD83C\uDD63\uD83C\uDD58\uD83C\uDD50\uD83C\uDD5B\uD83C\uDD58\uD83C\uDD62\uD83C\uDD58\uD83C\uDD5D\uD83C\uDD56");
+        System.out.println("Welcome to the game of Snap! The rules are simple, hit Enter to deal a card, if a match presents itself, then type snap to claim it and win, but don't miss your chance!");
+        System.out.println("The starting card is being dealt");
         Card firstCard = dealCard();
-        String playersTurn = "The First card has been drawn, player one take your turn";
+        String playersTurn = "The first card has been drawn, player one take your turn";
         String playersSwitch = "Player One this is your card";
         System.out.println(playersTurn);
         playersTurn = "Player One draw your card";
@@ -30,10 +33,9 @@ public class Snap extends CardGame {
             commands.getUserInput();
             Card newCard = dealCard();
             System.out.println(playersSwitch);
-            System.out.println("Press Enter to deal another card");
             if (firstCard.getCardFace().equals(newCard.getCardFace())) {
-                String userEnter = commands.winnerSnap();
-                if (userEnter.equals("snap")) {
+                String userInput = commands.winnerSnap();
+                if (userInput.equals("snap")) {
 
                     System.out.println("It's a match! Congratulations, you've won!");
                     System.out.println("Would you like to play again? Y/N");
@@ -50,7 +52,11 @@ public class Snap extends CardGame {
 
             playersSwitch = playersSwitch.equals("Player One this is your card") ? "Player Two this is your card" : "Player One this is your card";
             playersTurn = playersTurn.equals("Player One draw your card") ? "Player Two draw your card" : "Player One draw your card";
-            System.out.println(playersTurn);
+            if (faceMatch) {
+                System.out.println("Thanks for playing");
+            } else {
+                System.out.println(playersTurn);
+            }
         }
     }
 }
